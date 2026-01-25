17e8798de5812c390b7c3aceb893cbe81570257c
HYH <huyuhao612@outlook.com>
2026-01-25 23:30:30 +0800

    完成用户实体的创建

---
+++ models/UserBasic.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	Password      string
+	Phone         string
+	Email         string
+	Identity      string
+	ClientIp      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartBeatTime uint64
+	LogOutTime    uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
