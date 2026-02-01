cc96deeaa3541a9bc0f6516f860abd5337e33dfc
HYH <huyuhao612@outlook.com>
2026-02-01 23:18:04 +0800

    配置swagger

--- service/index.go
+++ service/index.go
@@ -2,6 +2,18 @@ package service
 
 import "github.com/gin-gonic/gin"
 
+// GetIndex
+// @Summary      得到信息p
+// @Description  get string by ID
+// @Tags         首页
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Account ID"
+// @Success      200  {string} 	welcome
+// @Failure      400  {string} 	welcome
+// @Failure      404  {string} 	welcome
+// @Failure      500  {string} 	welcome
+// @Router       /index [get]
 func GetIndex(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "p",
